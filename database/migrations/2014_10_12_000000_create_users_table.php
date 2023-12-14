fe845863ea4b9e596cd677a1f35e451707f423b7
firdaoesridwan <ridwanfirdaoes11@gmail.com>
2023-12-14 17:40:15 +0700

    migration dan laratrust

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('nama_lengkap');
+            $table->string('no_hp');
             $table->rememberToken();
             $table->timestamps();
         });
