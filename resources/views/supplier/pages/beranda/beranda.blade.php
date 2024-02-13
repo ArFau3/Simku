aca0b6c30d76afd670d8a22b0e21e9c5521304d2
firdaoesridwan <ridwanfirdaoes11@gmail.com>
2024-02-13 13:11:15 +0700

    bebas

--- resources/views/supplier/pages/beranda/beranda.blade.php
+++ resources/views/supplier/pages/beranda/beranda.blade.php
@@ -29,7 +29,7 @@
                         <div class="text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
                             <h2 class="text-3xl font-extrabold text-black sm:text-4xl">
                                 <span class="block">
-                                    Kosong ?
+                                    Kosong edit ?
                                 </span>
                                 <span class="block text-indigo-500 mt-3">
                                     Ya Benar. Masih kosong nih.
